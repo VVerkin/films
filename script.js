51d91c98543fd285f4e3065f8a47d5bbb7af2679
Vasilii <v.verkin@gmail.com>
2026-05-07 16:53:49 +0300

    Мелкие правки

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const personalMovieDB = {
   privat: false,
 };
 
-<<<<<<< HEAD
 function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
 
@@ -69,37 +68,8 @@ showMyDB(personalMovieDB.privat);
 function writeYourGenres () {
   for (let i = 1; i <= 3; i++) {
 
-    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-    personalMovieDB.genres[i - 1]
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
    
   }
 }
 
-=======
-or (let i = 0; i < 2; i++) {
-
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько его оцените?', '');
-
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log('done');
-  } else {
-      console.log('error');
-    i--;
-  }
-
-  if (personalMovieDB.count < 10) {
-    console.log('Просмотренно довольно мало фильмов');
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-  } else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-  } else {
-    console.log('Произошла ошибка');
-  }
-
-}
-
-console.log(personalMovieDB);
->>>>>>> 779f3bb89742e1aa93edfdce6730413d0ef9ed09
